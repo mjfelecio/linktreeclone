4d708b773faf9fdb6e95fbb48e240fe121d1cc40
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-03-28 10:55:24 +0800

    Added logout, login, and signout button on home

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+  devise_scope :user do
+    get "/users/sign_out" => "devise/sessions#destroy"
+  end
 end
