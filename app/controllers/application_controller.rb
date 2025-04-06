0e5492351d0ed3bc40d50e91ed287c96d1c824ac
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-04-06 15:40:09 +0800

    feat: Redirect new user to new tree
    The old behavior just redirected the user after logging in to the new_tree_path, which is intended. However, if they already have a tree, redirect the user to that tree instead.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # Redirect the user to their tree. Redirect to new_tree if no tree has been created
+  def after_sign_in_path_for(resource)
+    if current_user&.tree
+      tree_path(current_user.tree)
+    else
+      new_tree_path
+    end
+  end
 end
